3209c01e89b2f15a6196fd48deedf794500aa09f
Neil Slater <neil@aframe.com>
2013-07-29 10:16:22 +0100

    Add label attribute

--- lib/games_dice/explainer.rb
+++ lib/games_dice/explainer.rb
@@ -15,16 +15,33 @@ class GamesDice::Explainer
 
   # Creates new instance of GamesDice::Explainer.
   # @return [GamesDice::Explainer]
-  def initialize number, cause, details
-    @number = number
-    @cause = cause
+  def initialize label, number, cause, details
+    @label = label.to_s
+    @number = Integer(number)
+    @cause = cause.to_sym
     @details = details
   end
 
+  # @return [Integer] numeric value that is being explained
   attr_reader :number
 
+  # @return [Symbol] semantics of how the details are combined
   attr_reader :cause
 
+  # @return [Symbol] an array, either of "deeper" explanations, or of Integers
   attr_reader :details
+
+  # Counts number of explanation layers "deeper" than this one. A value of 0 means that this
+  # explanation has no deeper cause than the results of simple die rolls.
+  # @return [Integer] degree of deeper explanations
+  def content_depth
+    @content_depth ||= calc_content_depth
+  end
+
+  private
+
+  def calc_content_depth
+
+  end
 
 end # class GamesDice::Explainer
