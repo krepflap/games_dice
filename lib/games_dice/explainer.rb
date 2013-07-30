19c242fed5f808d439dfa47f6c625ad35c407e21
Neil Slater <neil@aframe.com>
2013-07-30 08:51:04 +0100

    Initial tests for explainer

--- lib/games_dice/explainer.rb
+++ lib/games_dice/explainer.rb
@@ -12,7 +12,6 @@
 #
 
 class GamesDice::Explainer
-
   # Creates new instance of GamesDice::Explainer.
   # @return [GamesDice::Explainer]
   def initialize label, number, cause, details
@@ -32,16 +31,32 @@ class GamesDice::Explainer
   attr_reader :details
 
   # Counts number of explanation layers "deeper" than this one. A value of 0 means that this
-  # explanation has no deeper cause than the results of simple die rolls.
+  # explanation has no deeper cause than the results of combining individual die rolls.
   # @return [Integer] degree of deeper explanations
   def content_depth
     @content_depth ||= calc_content_depth
   end
 
+  # @return [Array<Hash>]
+  # Types of explanation:
+  # top_down vs bottom_up
+  # depth_first vs breadth_first
+  # flat vs layered
+  # Each entry: an id, a parent id, a number, a label, how it contributes to an "upper" layer, how it derives from a "lower" layer
+
   private
 
   def calc_content_depth
-
+    return recurse_max_depth( details, 0 )
   end
 
+  def recurse_max_depth current_details, current_depth
+    current_details.map do |detail|
+      case detail
+      when Fixnum then current_depth
+      when GamesDice::DieResult then current_depth + ( detail.rolls.size > 1 ? 1 : 0 )
+      when GamesDice::Explainer then recurse_max_depth( detail.details, current_depth + 1 )
+      end
+    end.max
+  end
 end # class GamesDice::Explainer
--- spec/explainer_spec.rb
+++ spec/explainer_spec.rb
@@ -1,6 +1,41 @@
-require 'games_dice'
 require 'helpers'
 
 describe GamesDice::Explainer do
+  describe "class method" do
+    describe "#new" do
+      it "should instantiate if provided with valid parameters" do
+        GamesDice::Explainer.new( '3d6', 12, :sum, [1,5,6] ).should be_a GamesDice::Explainer
+      end
+    end
+  end # describe "class method"
+
+  describe "instance method" do
+    let( :ge_simple ) { GamesDice::Explainer.new( '3d6', 12, :sum, [1,5,6] ) }
+    let( :ge_complex01 ) { GamesDice::Explainer.new( '3d6', 12, :sum, [
+        1,5, GamesDice::Explainer.new('1d6', 6, :reroll, [4,5,6] ) ] ) }
+    let( :ge_complex02 ) { GamesDice::Explainer.new( '3d6', 12, :sum, [
+        1,5, GamesDice::DieResult.new(6) ] ) }
+    let( :ge_complex03 ) {
+      result = GamesDice::DieResult.new(6)
+      result.add_roll( 6, :reroll_replace )
+      GamesDice::Explainer.new( '3d6', 12, :sum, [
+        1,5, result ] ) }
+    let( :ge_complex04 ) { GamesDice::Explainer.new( 'weird', 36, :sum, [ge_complex03, ge_simple, ge_complex01 ] ) }
+
+    describe "#content_depth" do
+      it "should return 0 for a simple explanation" do
+        ge_simple.content_depth.should == 0
+        ge_complex02.content_depth.should == 0
+      end
+
+      it "should return corrrect numbers for more complex explanations" do
+        ge_complex01.content_depth.should == 1
+        ge_complex03.content_depth.should == 1
+        ge_complex04.content_depth.should == 2
+      end
+    end
+
+
+  end # describe "instance method"
 
 end # describe GamesDice::Explainer
